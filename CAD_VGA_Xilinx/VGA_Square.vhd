b9807308918e2ce3eefaee8c457b457b908ba959
FaryarD <amindaemdoost@yahoo.com>
2022-01-23 00:27:43 +0330

    show tank from player position pl_posx and pl_posy

--- CAD_VGA_Xilinx/VGA_Square.vhd
+++ CAD_VGA_Xilinx/VGA_Square.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
+use IEEE.NUMERIC_STD.all;
 
 entity VGA_Square is
   port ( CLK_24MHz		: in std_logic;
@@ -17,23 +17,48 @@ end VGA_Square;
 architecture Behavioral of VGA_Square is
 	type GROUND_STATE is (PLAYER,EMPTY,ICE,BRICK,WALL);
 	type GROUND_TYPE is array (0 to 19, 0 to 19) of GROUND_STATE ;
+	type POSITION_ARRAY is array (0 to 20) of integer ;
+	type BITMAP is array (0 to 21, 0 to 21) of bit ;
+	signal posx,posy,pl_posx,pl_posy: integer := 0;
 	
-	signal posx: integer := 0;
-	signal posy: integer  := 0;
 	signal ground: GROUND_TYPE := ((others=> (others=>EMPTY)));
   signal ColorOutput: std_logic_vector(5 downto 0);
   
-  signal SquareX: std_logic_vector(9 downto 0) := "0000001110";  
-  signal SquareY: std_logic_vector(9 downto 0) := "0000010111";  
-  signal SquareXMoveDir, SquareYMoveDir: std_logic := '0';
+ 
   --constant SquareWidth: std_logic_vector(4 downto 0) := "11001";
-  constant SquareXmin: std_logic_vector(9 downto 0) := "0000000001";
-  signal SquareXmax: std_logic_vector(9 downto 0); -- := "1010000000"-SquareWidth;
-  constant SquareYmin: std_logic_vector(9 downto 0) := "0000000001";
-  signal SquareYmax: std_logic_vector(9 downto 0); -- := "0111100000"-SquareWidth;
-  signal ColorSelect: std_logic_vector(2 downto 0) := "001";
+  
   signal Prescaler: std_logic_vector(30 downto 0) := (others => '0');
-  signal preskylinex,preskyliney: std_logic_vector(10 downto 0) := (others => '0');
+
+	constant startPositionsX : POSITION_ARRAY := (100,122,144,166,188,210,232,254,276,296,320,342,364,386,408,430,452,474,496,518,540);
+	constant startPositionsY : POSITION_ARRAY := (20,42,64,86,108,130,152,174,196,218,240,262,284,306,328,350,372,394,416,438,460);
+
+	--------------------------bit maps
+	constant tank : BITMAP :=(('0','0','0','0','0','0','0','0','0','0','1','1','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','1','1','1','1','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','1','1','1','1','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','1','1','1','1','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','1','1','1','1','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','1','1','1','1','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','1','1','1','1','0','0','0','0','0','0','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'));
+
+
+
+
 
 begin
 	
@@ -42,52 +67,16 @@ begin
 	ground(1,1)<=BRICK;
 	ground(9,9)<=BRICK;
 	ground(10,3)<=PLAYER;
+	pl_posx<=10;
+	pl_posy<=6;
 	PrescalerCounter: process(CLK_24MHz, RESET)
 	begin
 		if RESET = '1' then
 			Prescaler <= (others => '0');
-			--posPreScaler  <= (others => '0');
-			SquareX <= "0111000101";
-			SquareY <= "0001100010";
-			SquareXMoveDir <= '0';
-			SquareYMoveDir <= '0';
-			ColorSelect <= "001";
 		elsif rising_edge(CLK_24MHz) then
 			Prescaler <= Prescaler + 1;	 
 			if Prescaler = "11000011010100000" then  -- Activated every 0,002 sec (2 msec)
-				if SquareXMoveDir = '0' then
-					if SquareX < SquareXmax then
-						SquareX <= SquareX + 1;
-					else
-						SquareXMoveDir <= '1';
-						ColorSelect <= ColorSelect(1 downto 0) & ColorSelect(2);
-					end if;
-				else
-					if SquareX > SquareXmin then
-						SquareX <= SquareX - 1;
-					else
-						SquareXMoveDir <= '0';
-						ColorSelect <= ColorSelect(1 downto 0) & ColorSelect(2);
-					end if;	 
-				end if;
-		  
-				if SquareYMoveDir = '0' then
-					if SquareY < SquareYmax then
-						SquareY <= SquareY + 1;
-					else
-						SquareYMoveDir <= '1';
-						ColorSelect <= ColorSelect(1 downto 0) & ColorSelect(2);
-					end if;
-				else
-					if SquareY > SquareYmin then
-						SquareY <= SquareY - 1;
-					else
-						SquareYMoveDir <= '0';
-						ColorSelect <= ColorSelect(1 downto 0) & ColorSelect(2);
-					end if;	 
-				end if;		  
-			
-				Prescaler <= (others => '0');
+				
 			end if;
 		end if;
 	end process PrescalerCounter; 
@@ -165,10 +154,29 @@ begin
 				else 19 when ScanlineY>=conv_std_logic_vector(438,10) and ScanlineY<conv_std_logic_vector(460,10);
 				
 
-	ColorOutput <= "101010" when (ScanlineY>="0000000000" and ScanlineY<"0000010100") or (ScanlineX>="0000000000" and ScanlineX<"0001100100") or (ScanlineY>="0111001100" and ScanlineY<"0111100000" )or (ScanlineX>="1000011100" and ScanlineX<"1010000000")
-						else "000000" when ground(posy,posx)=EMPTY
-						else "110000" when ground(posy,posx)=BRICK
-						else "111111";
+--	ColorOutput <= "101010" when (ScanlineY>="0000000000" and ScanlineY<"0000010100") or (ScanlineX>="0000000000" and ScanlineX<"0001100100") or (ScanlineY>="0111001100" and ScanlineY<"0111100000" )or (ScanlineX>="1000011100" and ScanlineX<"1010000000")
+--						else "000000" when ground(posy,posx)=EMPTY
+--						else "110000" when ground(posy,posx)=BRICK
+--						else "111111";
+	
+	ColorProcess: process(ScanlineX,ScanlineY)
+	begin
+		if (ScanlineY>="0000000000" and ScanlineY<"0000010100") or (ScanlineX>="0000000000" and ScanlineX<"0001100100") or (ScanlineY>="0111001100" and ScanlineY<"0111100000" )or (ScanlineX>="1000011100" and ScanlineX<"1010000000") then
+			ColorOutput <= "101010";
+		elsif ground(posy,posx)=EMPTY then
+			ColorOutput <= "000000";
+		elsif ground(posy,posx)=BRICK then
+			ColorOutput <= "011000";
+		else
+			ColorOutput <= "000000";
+		end if;
+		if pl_posx=posx and pl_posy=posy then
+			--if tank(CONV_INTEGER(ScanlineY-std_logic_vector((to_unsigned(posy,10)* "0000010110"))-"0000010100"),CONV_INTEGER(ScanlineX-std_logic_vector((to_unsigned(posx,10)* "0000010110"))-"0001100100"))='1' then
+			if tank(CONV_INTEGER(ScanlineY-conv_std_logic_vector(startPositionsY(posy),10)),CONV_INTEGER(ScanlineX-conv_std_logic_vector(startPositionsX(posx),10)))='1' then
+				ColorOutput <= "100101";
+			end if;
+		end if;
+	end process ColorProcess; 
 	ColorOut <= ColorOutput;
 	
 	SquareXmax <= "1010000000"-SquareWidth; -- (640 - SquareWidth)
