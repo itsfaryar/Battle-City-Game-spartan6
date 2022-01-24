c0cc84c82d6f3c0c5358e7e01057f855cd51e40d
FaryarD <amindaemdoost@yahoo.com>
2022-01-24 19:01:18 +0330

    shooting

--- CAD_VGA_Xilinx/VGA_Square.vhd
+++ CAD_VGA_Xilinx/VGA_Square.vhd
@@ -31,7 +31,7 @@ architecture Behavioral of VGA_Square is
 	signal bulletOut : bit := '0';
 	signal ground: GROUND_TYPE := ((others=> (others=>EMPTY)));
 	signal ColorOutput: std_logic_vector(5 downto 0);
-	signal gameEnded,win,init : bit:='0';
+	signal gameEnded,win,init,gameStart : bit:='0';
 	signal countForoneSeccond : std_logic_vector(25 downto 0) := (others => '0');
   --constant SquareWidth: std_logic_vector(4 downto 0) := "11001";
 	signal sevensegmentStates : bit_vector(3 downto 0):= "0001";
@@ -138,6 +138,28 @@ constant tank_right : BITMAP := (('0','0','0','0','0','0','0','0','0','0','0','0
 ('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
 ('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'));
 
+constant bullet : BITMAP := (('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','1','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','1','1','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','1','1','1','1','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','1','1','1','1','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','1','1','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'),
+('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'));
 
 
 
@@ -147,7 +169,7 @@ constant tank_right : BITMAP := (('0','0','0','0','0','0','0','0','0','0','0','0
 begin
 
 	initialize: process(CLK_24MHz, RESET)
-variable x,y,bullet_newy,bullet_newx : integer :=0;
+variable x,y: integer :=0;
 		function lfsr32(x : std_logic_vector(31 downto 0)) return std_logic_vector is
 				begin
 				return x(30 downto 0) & (x(0) xnor x(1) xnor x(21) xnor x(31));
@@ -159,8 +181,8 @@ variable x,y,bullet_newy,bullet_newx : integer :=0;
 			y:=0;
 			ground <= ((others=> (others=>EMPTY)));
 			bulletdelay <= (others=>'0');
+			bulletOut<='0';
 		elsif rising_edge(CLK_24MHz) then
-		
 			if init='1' then
 				
 				pseudo_rand <= lfsr32(pseudo_rand);
@@ -190,58 +212,59 @@ variable x,y,bullet_newy,bullet_newx : integer :=0;
 				end if;
 --			ground(19,CONV_INTEGER(pseudo_rand(10 downto 5))rem 19) <= FLAG;
 			else
-				
-				if sw(0)='0' and bulletOut='0' then
-							bullet_dir<=player_dir;
-							bullet_x<=pl_posx;
-							bullet_y<=pl_posy;
-							bulletOut<='1';
-				else 
-					if bulletOut='1' then
-						bulletdelay<=bulletdelay+1;
-						if ground(bullet_y,bullet_x)=WALL then
-							bulletOut<='0';
-						elsif ground(bullet_y,bullet_x)=BRICK then
-							ground(bullet_y,bullet_x)<=crackedBrick;
-							bulletOut<='0';
-						elsif ground(bullet_y,bullet_x)=crackedBrick then
-							ground(bullet_y,bullet_x)<=EMPTY;
-							bulletOut<='0';
-						end if;
-				
-							if (bulletdelay = "100110001001011010000000") then 
-								bulletdelay<=(others=>'0');
-									
-									case bullet_dir is
-										when UP =>
-											if bullet_y>0 then
-												bullet_y<=bullet_y-1;
-											else
-												bulletOut<='0';
-											end if;
-										when DOWN =>
-											if bullet_y<19 then
-												bullet_y<=bullet_y+1;
-											else
-												bulletOut<='0';
-											end if;
-										when LEFT =>
-											if bullet_x>0 then 
-												bullet_x<=bullet_x-1;
-											else
-												bulletOut<='0';
-											end if;
-										when others => -- right
-										if bullet_x<19 then
-											bullet_x<=bullet_x+1;
-										else
-											bulletOut<='0';
-										end if;
-									end case;
-								
+				if gameStart='1' then
+					if sw(0)='0' and bulletOut='0' then
+								bullet_dir<=player_dir;
+								bullet_x<=pl_posx;
+								bullet_y<=pl_posy;
+								bulletOut<='1';
+					else 
+						if bulletOut='1' then
+							bulletdelay<=bulletdelay+1;
+							if ground(bullet_y,bullet_x)=WALL then
+								bulletOut<='0';
+							elsif ground(bullet_y,bullet_x)=BRICK then
+								ground(bullet_y,bullet_x)<=crackedBrick;
+								bulletOut<='0';
+							elsif ground(bullet_y,bullet_x)=crackedBrick then
+								ground(bullet_y,bullet_x)<=EMPTY;
+								bulletOut<='0';
 							end if;
-						end if;
-					end if;-- if bulletOut
+					
+								if (bulletdelay = "100110001001011010000000") then 
+									bulletdelay<=(others=>'0');
+										
+										case bullet_dir is
+											when UP =>
+												if bullet_y>0 then
+													bullet_y<=bullet_y-1;
+												else
+													bulletOut<='0';
+												end if;
+											when DOWN =>
+												if bullet_y<19 then
+													bullet_y<=bullet_y+1;
+												else
+													bulletOut<='0';
+												end if;
+											when LEFT =>
+												if bullet_x>0 then 
+													bullet_x<=bullet_x-1;
+												else
+													bulletOut<='0';
+												end if;
+											when others => -- right
+											if bullet_x<19 then
+												bullet_x<=bullet_x+1;
+											else
+												bulletOut<='0';
+											end if;
+										end case;
+									 
+								end if;
+							end if;
+						end if;-- if bulletOut
+					end if;--gameStart='1'
 			end if;
 		end if;
 	end process initialize; 
@@ -285,70 +308,70 @@ variable x,y,bullet_newy,bullet_newx : integer :=0;
 	segout <= sevensegmentStates;
 	sevenseg <= sevensegmentOut;
 	
-	timer: process(CLK_24MHz, RESET)
-	begin
-		if RESET = '1' then
-			countForoneSeccond <= (others => '0');
-			time_s <= 0;
-			time_s_10 <= 0;
-			time_s_01 <= 0;
-			time_m_10 <= 0;
-			time_m_01 <= 0;
-			time_m <= 0;
-			gameEnded<= '0';
-			win <= '0';
-		elsif rising_edge(CLK_24MHz) then
-			if init='0' then
-				if ground(pl_posy,pl_posx)=FLAG then
-					gameEnded <= '1';
-				end if;
-				if gameEnded='0' then
-					countForoneSeccond <= countForoneSeccond+1;
-					if countForoneSeccond = "1011011100011011000000000" then  -- Activated every 0,002 sec (2 msec)
-						if time_s<59 then
-							time_s <= time_s+1;
-							if time_s_01<9 then
-								time_s_01<= time_s_01+1;
-							else
-								time_s_01<=0;
-								time_s_10<=time_s_10+1;
-							end if;
-						else
-							time_s <= 0;
-							time_s_01<=0;
-							time_s_10<=0;
-							if time_m<=59 then
-								time_m<=time_m+1;
-								if time_m_01<9 then
-									time_m_01<= time_m_01+1;
-								else
-									time_m_01<=0;
-									time_m_10<=time_m_10;
-								end if;
-							else
-								time_m <=0;
-								time_m_01<=0;
-								time_m_10<=0;
-								gameEnded <= '1';
-							end if;
-							
-						end if;
-						countForoneSeccond <= (others => '0');
-					end if;
-					elsif time_m<10 then
-						win <= '1';
-						
-				end if;
-			end if;
-		end if;
-	end process timer; 
+--	timer: process(CLK_24MHz, RESET)
+--	begin
+--		if RESET = '1' then
+--			countForoneSeccond <= (others => '0');
+--			time_s <= 0;
+--			time_s_10 <= 0;
+--			time_s_01 <= 0;
+--			time_m_10 <= 0;
+--			time_m_01 <= 0;
+--			time_m <= 0;
+--			gameEnded<= '0';
+--			win <= '0';
+--		elsif rising_edge(CLK_24MHz) then
+--			if init='0' then
+--				if ground(pl_posy,pl_posx)=FLAG then
+--					gameEnded <= '1';
+--				end if;
+--				if gameEnded='0' then
+--					countForoneSeccond <= countForoneSeccond+1;
+--					if countForoneSeccond = "1011011100011011000000000" then  -- Activated every 0,002 sec (2 msec)
+--						if time_s<59 then
+--							time_s <= time_s+1;
+--							if time_s_01<9 then
+--								time_s_01<= time_s_01+1;
+--							else
+--								time_s_01<=0;
+--								time_s_10<=time_s_10+1;
+--							end if;
+--						else
+--							time_s <= 0;
+--							time_s_01<=0;
+--							time_s_10<=0;
+--							if time_m<=59 then
+--								time_m<=time_m+1;
+--								if time_m_01<9 then
+--									time_m_01<= time_m_01+1;
+--								else
+--									time_m_01<=0;
+--									time_m_10<=time_m_10+1;
+--								end if;
+--							else
+--								time_m <=0;
+--								time_m_01<=0;
+--								time_m_10<=0;
+--								gameEnded <= '1';
+--							end if;
+--							
+--						end if;
+--						countForoneSeccond <= (others => '0');
+--					end if;
+--					elsif time_m<10 then
+--						win <= '1';
+--						
+--				end if;
+--			end if;
+--		end if;
+--	end process timer; 
 
 	
 	PrescalerCounter: process(CLK_24MHz, RESET)
 		
 	begin
 		if RESET = '1' then
-			
+			gameStart<='0';
 			Prescaler <= (others => '0');
 			
 	      pl_posx <=0;
@@ -367,6 +390,7 @@ variable x,y,bullet_newy,bullet_newx : integer :=0;
 				
 					if Prescaler = "100110001001011010000000" then  -- Activated every 0,002 sec (2 msec)
 						 if key(0)='0' then
+							gameStart<= '1';
 							if pl_posy>0 and (ground(pl_posy-1,pl_posx)=EMPTY or ground(pl_posy-1,pl_posx)=ICE or ground(pl_posy-1,pl_posx)=FLAG) then
 								pl_posy<=pl_posy-1;
 								player_dir<=UP;
@@ -374,6 +398,7 @@ variable x,y,bullet_newy,bullet_newx : integer :=0;
 								player_dir<=DOWN;
 							end if;
 						elsif key(3)='0' then
+							gameStart<= '1';
 							if pl_posy<19 and (ground(pl_posy+1,pl_posx)=EMPTY or ground(pl_posy+1,pl_posx)=ICE or ground(pl_posy+1,pl_posx)=FLAG) then
 								pl_posy<=pl_posy+1;
 								player_dir<= DOWN;
@@ -381,6 +406,7 @@ variable x,y,bullet_newy,bullet_newx : integer :=0;
 								player_dir<= UP;
 							end if;
 						elsif key(1)='0' then
+							gameStart<= '1';
 							if pl_posx<19 and (ground(pl_posy,pl_posx+1)=EMPTY or ground(pl_posy,pl_posx+1)=ICE or ground(pl_posy,pl_posx+1)=FLAG)then
 								pl_posx<=pl_posx+1;
 								player_dir<= RIGHT;
@@ -388,6 +414,7 @@ variable x,y,bullet_newy,bullet_newx : integer :=0;
 								player_dir<= LEFT;
 							end if;
 							elsif key(2)='0' then
+								gameStart<= '1';
 							if pl_posx>0 and (ground(pl_posy,pl_posx-1)=EMPTY or ground(pl_posy,pl_posx-1)=ICE or ground(pl_posy,pl_posx-1)=FLAG) then
 								pl_posx<=pl_posx-1;
 								player_dir<= LEFT;
@@ -502,11 +529,11 @@ variable x,y,bullet_newy,bullet_newx : integer :=0;
 ---------methode 1
 	ColorOutput <= "101010" when (ScanlineY>="0000000000" and ScanlineY<"0000010100") or (ScanlineX>="0000000000" and ScanlineX<"0001100100") or (ScanlineY>="0111001100" and ScanlineY<"0111100000" )or (ScanlineX>="1000011100" and ScanlineX<"1010000000")
 						else "001000" when win='1'
-						else "100101" when init='0' and player_dir=UP and pl_posx=posx and pl_posy=posy and tank_up(CONV_INTEGER(ScanlineY-conv_std_logic_vector(startPositionsY(posy),10)),CONV_INTEGER(ScanlineX-conv_std_logic_vector(startPositionsX(posx),10)))='1'
-						else "100101" when init='0' and player_dir=DOWN and pl_posx=posx and pl_posy=posy and tank_down(CONV_INTEGER(ScanlineY-conv_std_logic_vector(startPositionsY(posy),10)),CONV_INTEGER(ScanlineX-conv_std_logic_vector(startPositionsX(posx),10)))='1'
-						else "100101" when init='0' and player_dir=LEFT and pl_posx=posx and pl_posy=posy and tank_left(CONV_INTEGER(ScanlineY-conv_std_logic_vector(startPositionsY(posy),10)),CONV_INTEGER(ScanlineX-conv_std_logic_vector(startPositionsX(posx),10)))='1'
-						else "100101" when init='0' and player_dir=RIGHT and pl_posx=posx and pl_posy=posy and tank_right(CONV_INTEGER(ScanlineY-conv_std_logic_vector(startPositionsY(posy),10)),CONV_INTEGER(ScanlineX-conv_std_logic_vector(startPositionsX(posx),10)))='1'
-						else "111111" when bullet_x=posx and bullet_y=posy and bulletOut='1'
+						else "100101" when gameStart='1' and player_dir=UP and pl_posx=posx and pl_posy=posy and tank_up(CONV_INTEGER(ScanlineY-conv_std_logic_vector(startPositionsY(posy),10)),CONV_INTEGER(ScanlineX-conv_std_logic_vector(startPositionsX(posx),10)))='1'
+						else "100101" when gameStart='1' and player_dir=DOWN and pl_posx=posx and pl_posy=posy and tank_down(CONV_INTEGER(ScanlineY-conv_std_logic_vector(startPositionsY(posy),10)),CONV_INTEGER(ScanlineX-conv_std_logic_vector(startPositionsX(posx),10)))='1'
+						else "100101" when gameStart='1' and player_dir=LEFT and pl_posx=posx and pl_posy=posy and tank_left(CONV_INTEGER(ScanlineY-conv_std_logic_vector(startPositionsY(posy),10)),CONV_INTEGER(ScanlineX-conv_std_logic_vector(startPositionsX(posx),10)))='1'
+						else "100101" when gameStart='1' and player_dir=RIGHT and pl_posx=posx and pl_posy=posy and tank_right(CONV_INTEGER(ScanlineY-conv_std_logic_vector(startPositionsY(posy),10)),CONV_INTEGER(ScanlineX-conv_std_logic_vector(startPositionsX(posx),10)))='1'
+						else "111111" when bullet_x=posx and bullet_y=posy and bulletOut='1' and bullet(CONV_INTEGER(ScanlineY-conv_std_logic_vector(startPositionsY(posy),10)),CONV_INTEGER(ScanlineX-conv_std_logic_vector(startPositionsX(posx),10)))='1'
 						else "000000" when ground(posy,posx)=EMPTY
 						else "000001" when ground(posy,posx)=ICE
 						else "101010" when ground(posy,posx)=WALL
